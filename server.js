0a252a9c591d0b535ed6879dd46bedc68c3760a3
rabeea jahangir <rabeeajahangir@gmail.com>
2021-09-26 21:02:18 -0400

    added changes to post

--- server.js
+++ server.js
@@ -1,23 +1,42 @@
-const express = require('express');
- const multer = require("multer");
+const path = require("path");
+
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
+
 const app = express();
 
-const fileStorageEngine = multer.diskStorage ({
-  destination: (req, file, cb) =>{
-    cb(null, './images');
+// This middleware is used to enable Cross Origin Resource Sharing This sets Headers to allow access to our client application
+app.use(cors());
+
+// Storage Engine That Tells/Configures Multer for where (destination) and how (filename) to save/upload our files
+const fileStorageEngine = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./images"); 
   },
-  finename: (req, file, cb) => {
-    cb(null, Date.now() + '--' + file.originalname);
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "--" + file.originalname);
   },
 });
 
-app.post('/single', upload.single('image'), (req, res) => {
+// Route To Load Index.html page to browser
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+
+const upload = multer({ storage: fileStorageEngine });
+
+// Single File Route Handler
+app.post("/single", upload.single("image"), (req, res) => {
   console.log(req.file);
-  res.send('Single File upload success');
+  res.send("Single File upload success");
 });
 
-const upload = multer({storage: fileStorageEngine});
-
+// Multiple Files Route Handler
+app.post("/multiple", upload.array("images", 3), (req, res) => {
+  console.log(req.files);
+  res.send("Multiple Files Upload Success");
+});
 
 app.listen(5000);
-
